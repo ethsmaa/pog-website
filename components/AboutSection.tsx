81ceb8b6a640da9b3f415b9d7ef76bf7959b47d2
Elif <elifbaser05@gmail.com>
2024-09-08 21:28:28 +0300

    Refactor: Update AboutSection component to add responsive background gradients

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -1,24 +1,49 @@
 import React from 'react';
-import Image from 'next/image';
+import rightGradient from '/public/about-rm.svg'; // Gradient png dosyasının yolu
+import leftGradient from '/public/about-lb.svg'; // Köşe png dosyasının yolu
 
 export default function AboutSection() {
   return (
-    <div className="relative flex justify-center items-center h-screen w-screen bg-gray-100">
-      {/* Burada resmi tam genişlik ve yükseklik olarak ayarlıyoruz */}
-      <div className="relative w-full h-full">
-        <Image
-          src="/about-us-background.png"
-          alt="About Section Background"
-          layout="fill"
-          objectFit="cover"
+    <div
+      className="about-us-section"
+      style={{
+        backgroundColor: 'white', // Beyaz arka plan
+        position: 'relative', // Gradient'leri belirli konumlarda yerleştirebilmek için
+        width: '100vw',
+        height: '100vh',
+        overflow: 'hidden', // Gradient taşmasını önlemek için
+      }}
+    >
+      {/* main gradient */}
+      <div
+        style={{
+          position: 'absolute',
+          top: '0%',
+          right: '0%',
+          width: '700px', // Boyutu daha da büyüttüm
+          height: '700px', // Boyutu daha da büyüttüm
+          backgroundImage: `url(${rightGradient.src})`,
+          backgroundSize: 'contain',
+          backgroundRepeat: 'no-repeat',
+        }}
+      />
 
-          objectPosition="center"
-        />
-        <div className="absolute inset-0 flex items-center justify-center">
-          <h1 className="text-white text-4xl">Burasi About Kismi</h1>
-          <h2>POG</h2>
-        </div>
-      </div>
+      {/* Gradient sol alt köşeye yakın bir yerde */}
+      <div
+        style={{
+          position: 'absolute',
+          bottom: '-61%',
+          left: '-17%',
+          width: '700px',
+          height: '700px',
+          backgroundImage: `url(${leftGradient.src})`,
+          backgroundSize: 'contain',
+          backgroundRepeat: 'no-repeat',
+          opacity: 0.6,
+        }}
+      />
+
+      
     </div>
   );
 }
