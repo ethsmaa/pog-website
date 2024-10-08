bdfe61194f4bfc7313df1101fadfcc41e1dcf6d5
esma <esmaoruccs@gmail.com>
2024-10-08 11:43:49 +0300

    refactor: sponsor design

--- components/SponsorSection.tsx
+++ components/SponsorSection.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { FaQuoteLeft } from "react-icons/fa";
 
 export default function SponsorSection() {
     return (
-       <div id='sponsors' className="bg-[#5585B6] w-screen h-[200px] flex items-center justify-center">
-            <div className='flex justify-center flex-col items-center text-white space-y-4'>
-               <div className='italic'>&quot;Video oyunları sadece etkileşimli bir sanat biçimi değil, başka bir insanın gözlerinden yaşamamızı sağlayan bir yoldur&quot;</div>
-               <div>Hideo Kojima</div>
-            </div>      
+       <div id='sponsors' className="bg-[#71A5CA] w-screen flex items-center justify-center text-white">
+            <div className='p-10 flex justify-center items-center flex-col space-y-2 py-32x '>
+               <FaQuoteLeft className='self-start text-[30px]'/>
+               <div className='w-5/6 md:w-3/4 text-center'>Video oyunları sadece etkileşimli bir sanat biçimi değil, başka bir insanın gözlerinden yaşamamızı sağlayan bir yoldur.</div>
+               <div className='italic'>Hideo Kojima</div>
+
+
+            </div>
        </div>
     );
 }
