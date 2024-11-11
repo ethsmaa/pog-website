48285ed07835a9eeceae9a1eec8b337f77e6c590
esma <esmaoruccs@gmail.com>
2024-11-11 17:53:51 +0300

    notion content format

--- components/EventSection.tsx
+++ components/EventSection.tsx
@@ -2,34 +2,28 @@ import React, { useState } from "react";
 import leftGradient from "../public/images/event-lb.svg";
 import rightGradient from "../public/images/event-rm.svg";
 
-
-
 import { EventBox } from "./EventBox";
 import { PageCard } from "./PageCard";
 import { EVENTS_DATA } from "@/utils/constants";
 
 export default function EventSection({ events }: { events: EventData[] }) {
-  const [selectedEvent, setSelectedEvent] = useState<number | null>(null); // Seçilen event index'i
+  const [selectedEvent, setSelectedEvent] = useState<number | null>(null);
 
   return (
     <div
       id="events"
       className="relative w-screen min-h-screen overflow-hidden bg-white dark:bg-gray-900"
     >
-      {/* Sağ arka plan gradyanı */}
+      {/* Right background gradient */}
       <div
         className="absolute top-[7%] right-[-10%] w-[700px] h-[800px] bg-no-repeat bg-contain opacity-60 md:opacity-80 z-0"
-        style={{
-           backgroundImage: `url(${rightGradient.src})`,
-        }}
+        style={{ backgroundImage: `url(${rightGradient.src})` }}
       />
 
-      {/* Sol arka plan gradyanı */}
+      {/* Left background gradient */}
       <div
         className="absolute bottom-[1%] left-[-15%] w-[750px] h-[700px] bg-no-repeat bg-contain opacity-70 z-0"
-        style={{
-          backgroundImage: `url(${leftGradient.src})`,
-        }}
+        style={{ backgroundImage: `url(${leftGradient.src})` }}
       />
 
       <div className="absolute inset-0 flex pt-2 md:pt-0 md:items-center justify-center">
@@ -50,10 +44,9 @@ export default function EventSection({ events }: { events: EventData[] }) {
                 key={index}
                 event={event}
                 isSelected={selectedEvent === index}
-                onClick={() =>
-                  setSelectedEvent(selectedEvent === index ? null : index)
-                } // Seçiliyse iptal et, değilse seç
-                isHidden={selectedEvent !== null && selectedEvent !== index} // Seçili olmayanlar görünmez
+                onClick={() => setSelectedEvent(index)} 
+                onClose={() => setSelectedEvent(null)} 
+                isHidden={selectedEvent !== null && selectedEvent !== index} // Hide non-selected events
               />
             ))}
           </div>
